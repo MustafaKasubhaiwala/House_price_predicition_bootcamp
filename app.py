7fc400b44081ba6f4e0303c86834805a7e606dd0
Mustafa Kasubhaiwala <mustafakasubai@gmail.com>
2025-06-11 22:58:34 +0530

    initial commits

--- app.py
+++ app.py
@@ -6,3 +6,26 @@ app = Flask(__name__)
 
 with open('house_price_prediction.pkl','rb') as f:
     model.pickle.load(f)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',method=['POST'])
+def predict():
+    features = [
+        float(request.form['CRIM']),
+        float(request.form['ZN']),
+        float(request.form['INDUS']),
+        float(request.form['CHAS']),
+        float(request.form['NOX']),
+        float(request.form['RM']),
+        float(request.form['AGE']),
+        float(request.form['DIS']),
+        float(request.form['RAD']),
+        float(request.form['TAX']),
+        float(request.form['PTRATIO']),
+        float(request.form['B']),
+        float(request.form['LSTAT'])
+
+    ]
